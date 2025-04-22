e1c7b60226e5891323d788a82917400b6fe973ca
keshav1499 <150837148+keshav1499@users.noreply.github.com>
2025-04-22 09:31:21 +0530

    update marquee

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,18 +1,13 @@
-
 import React from 'react'
 
 function Marquee({text}) {
   return (
     <div >
-      <div className='bg-primary p-1 text-light fw-bold shadow lg:text-2xl text-lg' >
+      <div className='bg-green-600 p-1 text-light fw-bold shadow lg:text-2xl text-lg' >
         <marquee behavior="scroll" direction="left">
-
-        {text}
-
+          {text}
         </marquee>
-
       </div>
-    
     </div>
   )
 }
